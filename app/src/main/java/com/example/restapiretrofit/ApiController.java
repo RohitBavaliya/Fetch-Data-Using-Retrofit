802f3ea3a88f86d63982c5a70f04d8823858689f
rohit <rohit13bavaliya@gmail.com>
2021-07-06 20:37:18 +0530

    add comments

--- app/src/main/java/com/example/restapiretrofit/ApiController.java
+++ app/src/main/java/com/example/restapiretrofit/ApiController.java
@@ -5,6 +5,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiController
 {
+    /*
+    * 1. Step-1 : define url
+    * 2. Step-2 : Create an Objects of ApiController and Retrofit
+    * 3. Step-3 : Create Constructor of ApiController and call retrofit object inside
+    * 4. Step-4 : If ApiController object is null than create new object
+    * 5. Step-5 : Call all Api
+    * */
     private final static String url = "http://192.168.46.146/api/";
     private static ApiController apiController;
     private static Retrofit retrofit;
